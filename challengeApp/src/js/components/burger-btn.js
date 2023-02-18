98e045948b562447cc87c2db69a9682ba70c4a22
Maubry94 <matthieu.aubry94370@gmail.com>
2023-02-18 21:42:58 +0100

    [UPDATE] : Add click outside event to close menu

--- challengeApp/src/js/components/burger-btn.js
+++ challengeApp/src/js/components/burger-btn.js
@@ -2,9 +2,12 @@ const burgerBtn = document.querySelector(".burger-button");
 const burgerWrapper = document.querySelector(".burger-wrapper");
 burgerBtn.addEventListener("click", burgerMenuToggle);
 
+let isOpen = false;
+
 function burgerMenuToggle() {
   burgerBtn.classList.toggle("active");
   burgerWrapper.classList.toggle("active");
+  isOpen = !isOpen;
 }
 
 window.addEventListener("resize", () => {
@@ -13,5 +16,14 @@ window.addEventListener("resize", () => {
   if (width > 768) {
     burgerBtn.classList.remove("active");
     burgerWrapper.classList.remove("active");
+    isOpen = false;
+  }
+});
+
+document.addEventListener('click', function(event) {
+  if (!burgerWrapper.contains(event.target) && !burgerBtn.contains(event.target) && isOpen) {
+    burgerBtn.classList.remove("active");
+    burgerWrapper.classList.remove("active");
+    isOpen = false;
   }
 });
