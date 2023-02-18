7ce1e35aad611395a183f7605f96fe9ece834f83
Maubry94 <matthieu.aubry94370@gmail.com>
2023-02-18 21:17:59 +0100

    [UPDATE] : Add mobile menu to header

--- challengeApp/src/js/components/burger-btn.js
+++ challengeApp/src/js/components/burger-btn.js
@@ -1,6 +1,17 @@
 const burgerBtn = document.querySelector(".burger-button");
+const burgerWrapper = document.querySelector(".burger-wrapper");
 burgerBtn.addEventListener("click", burgerMenuToggle);
 
 function burgerMenuToggle() {
   burgerBtn.classList.toggle("active");
+  burgerWrapper.classList.toggle("active");
 }
+
+window.addEventListener("resize", () => {
+  const width = window.innerWidth;
+
+  if (width > 768) {
+    burgerBtn.classList.remove("active");
+    burgerWrapper.classList.remove("active");
+  }
+});
